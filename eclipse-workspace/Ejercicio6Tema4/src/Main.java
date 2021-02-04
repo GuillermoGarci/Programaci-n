976736bdaa1956e65150fdc98fbd79b1aabf8996
GuillermoGarci <thewilliamtigers@gmail.com>
2021-02-04 12:14:10 +0100

    Añadiendo el ejercicio 6 del tema 4

---
+++ eclipse-workspace/Ejercicio6Tema4/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+  
+public class Main { 
+	public static void main(String[] args) { 
+		Nif nif1 = new Nif();  
+		 
+		nif1.leer(); 
+		System.out.println(nif1.toString()); 
+	} 
+} 
