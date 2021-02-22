7cda93a13a983e56ad421e0c6984cb69128e41f8
GuillermoGarci <72188271+GuillermoGarci@users.noreply.github.com>
2021-02-22 07:23:02 +0100

    Terminando el ejercicio

--- eclipse-workspace/Ejercicio1Tema5/src/Television.java
+++ eclipse-workspace/Ejercicio1Tema5/src/Television.java
@@ -1,3 +1,38 @@
 public class Television extends Electrodomestico{
+	private float pulgada;
+	private boolean sintonizadorTdt;
 	
+	public Television() {
+		this.pulgada = 20;
+		this.sintonizadorTdt = false;
+	}
+	
+	public Television(float precioBase, float peso) {
+		super(precioBase, peso);
+	}
+	
+	public Television(float pulgada, boolean sintonizadorTdt) {
+		super();
+		this.pulgada = pulgada;
+		this.sintonizadorTdt = sintonizadorTdt;
+	}
+
+	public float getPulgada() {
+		return pulgada;
+	}
+
+	public boolean getSintonizadorTdt() {
+		return sintonizadorTdt;
+	}
+	
+	public void precioFinal() {
+		super.precioFinal();
+		if(getPulgada() > 40) {
+			setPrecioBase((getPrecioBase()/100)*30);
+		}
+		
+		if(getSintonizadorTdt() == true) {
+			setPrecioBase(getPrecioBase() + 50);
+		}
+	}
 }
