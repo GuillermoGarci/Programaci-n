1fa7aed9adf45ae66285043de0434fdeb2f52ece
GuillermoGarci <72188271+GuillermoGarci@users.noreply.github.com>
2021-02-22 07:22:22 +0100

    Terminanado el ejercicio

--- eclipse-workspace/Ejercicio1Tema5/src/Lavadora.java
+++ eclipse-workspace/Ejercicio1Tema5/src/Lavadora.java
@@ -1,3 +1,27 @@
 public class Lavadora extends Electrodomestico{
+	private float carga;
 	
+	public Lavadora() {
+		this.carga = getCINCO();
+	}
+	
+	public Lavadora(float precioBase, float peso) {
+		super(precioBase, peso);
+	}
+	
+	public Lavadora(float carga) {
+		super();
+		this.carga = carga;
+	}
+
+	public float getCarga() {
+		return carga;
+	}
+	
+	public void precioFinal() {
+		super.precioFinal();
+		if(getCarga() > 30) {
+			setPrecioBase(getPrecioBase() + 50);
+		}
+	}
 }
